85ba9c2b4b7861b32c24ce35866afae57621ad64
PanPusty <ushejdjjd@gmail.com>
2026-04-24 13:34:46 +0200

    Add files via upload
    dodano

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>konfigurator samochodów</title>
+      <link rel="stylesheet" href="styl.css">
+
+</head>
+<body>
+<div class ="blok-nagłówkowy">
+<h1>Serwis konfiguracji samochodów</h1>
+</div>      
+<div class ="blok-nawigacyjny">
+<h2>samochody</h2>                   
+<h2>konfigurator</h2>
+<h2>kontakt</h2>
+</div>
+<main>
+<section class="sec1">
+  <table>
+    <tr></tr>
+    <tr></tr>
+    <tr></tr>
+    <tr></tr>
+
+  </table>    
+</section>
+<section class="sec2">
+      
+<table>  </table>
+
+
+
+</section>
+<section class="sec3"></section>
+
+</main>
+
+</body>
+</html>
